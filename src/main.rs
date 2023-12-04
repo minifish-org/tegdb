89efd6534b579e9858b43b17051288aa5b9e5c7a
Jack Yu <jackysp@gmail.com>
2023-12-04 20:47:36 +0800

    make long connections
    Signed-off-by: Jack Yu <jackysp@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -26,47 +26,59 @@ fn main() {
 
 // handle_connection is used to handle tcp request.
 fn handle_connection (mut stream: TcpStream, kv_data: &Arc<Mutex<BTreeMap<String, String>>>) {
+    let mut stream_for_reading = stream.try_clone().unwrap();
     let mut buffer = [0; 1024];
-    let mut reader = BufReader::new(&stream);
-    reader.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let mut reader = BufReader::new(&mut stream_for_reading);
+    loop {
+        match reader.read(&mut buffer) {
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    // The client has disconnected.
+                    break;
+                }
+                let request = String::from_utf8_lossy(&buffer[..]).to_string();
+                let data: Vec<&str> = request.split_whitespace().collect();
 
-    let request = String::from_utf8_lossy(&buffer[..]).to_string();
-    let data: Vec<&str> = request.split_whitespace().collect();
+                for i in 0..data.len() {
+                    println!("{}: {}", i, data[i]);
+                }
 
-    for i in 0..data.len() {
-        println!("{}: {}", i, data[i]);
-    }
+                let cmd = data[0];
+                match cmd {
+                    "g" => {
+                        if data.len() != 3 {
+                            let response = "NOT FOUND\n";
+                            stream.write(response.as_bytes()).unwrap();
+                            continue;
+                        }
+                        let key = data[1];
+                        let response = format!("{}\n", kv_data.lock().unwrap().get(key).unwrap_or(&"NOT FOUND".to_string()));
+                        stream.write(response.as_bytes()).unwrap();
+                    },
+                    "s" => {
+                        if data.len() != 4 {
+                            let response = "NOT FOUND\n";
+                            stream.write(response.as_bytes()).unwrap();
+                            continue;
+                        }
+                        let key = data[1];
+                        let value = data[2];
+                        kv_data.lock().unwrap().insert(key.to_string(), value.to_string());
+                        let response = "OK\n";
+                        stream.write(response.as_bytes()).unwrap();
+                    },
+                    _ => {
+                        let response = "NOT FOUND\n";
+                        stream.write(response.as_bytes()).unwrap();
+                    }
+                };
 
-    let cmd = data[0];
-    match cmd {
-        "g" => {
-            if data.len() != 3 {
-                let response = "NOT FOUND\n";
-                stream.write(response.as_bytes()).unwrap();
-                return; 
+                stream.flush().unwrap();
+            },
+            Err(e) => {
+                println!("Failed to read from connection: {}", e);
+                break;
             }
-            let key = data[1];
-            let response = format!("{}\n", kv_data.lock().unwrap().get(key).unwrap_or(&"NOT FOUND".to_string()));
-            stream.write(response.as_bytes()).unwrap();
-        },
-        "s" => {
-            if data.len() != 4 {
-                let response = "NOT FOUND\n";
-                stream.write(response.as_bytes()).unwrap();
-                return; 
-            }
-            let key = data[1];
-            let value = data[2];
-            kv_data.lock().unwrap().insert(key.to_string(), value.to_string());
-            let response = "OK\n";
-            stream.write(response.as_bytes()).unwrap();
-        },
-        _ => {
-            let response = "NOT FOUND\n";
-            stream.write(response.as_bytes()).unwrap();
         }
-    };
-
-    stream.flush().unwrap();
+    }
 }
