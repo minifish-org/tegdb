b25f2cecca220cd2dc718caaf62c90cbcdf03562
Jack Yu <jackysp@gmail.com>
2023-12-04 17:04:08 +0800

    make it could run
    Signed-off-by: Jack Yu <jackysp@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -7,34 +7,40 @@ use std::{
 fn main() {
     let mut kv_data = BTreeMap::new();
 
-    kv_data.insert("key1", "value1");
-    kv_data.insert("key2", "value2");
-
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection established!");
-        handle_connection(stream);
+        handle_connection(stream, &mut kv_data);
     }
-    if kv_data.contains_key("key1") {
-        println!("We've got {}", kv_data.get("key1").unwrap());
-    }
-    kv_data.remove("key1");
-    println!("We've got {}", kv_data.get("key2").unwrap());
-    println!("We've got {}", kv_data.get("key1").unwrap_or(&"default"));
-    println!("We've got {}", kv_data.get("key1").unwrap_or(&"default").to_uppercase());
-    println!("We've got {}", kv_data.get("key1").unwrap_or(&"default").to_ascii_lowercase());
-    println!("We've got {} keys", kv_data.len());
 }
 
 // handle_connection is used to handle tcp request.
-fn handle_connection (mut stream: TcpStream) {
+fn handle_connection (mut stream: TcpStream, kv_data: &mut BTreeMap<String, String>) {
     let mut buffer = [0; 1024];
     let mut reader = BufReader::new(&stream);
     reader.read(&mut buffer).unwrap();
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let request = String::from_utf8_lossy(&buffer[..]).to_string();
+    process_request(&request, kv_data);
     let response = "HTTP/1.1 200 OK\r\n\r\n";
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+fn process_request(request: &str, kv_data: &mut BTreeMap<String, String>) {
+    kv_data.insert("key".to_string(), request.to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process_request() {
+        let mut kv_data = BTreeMap::new();
+        process_request("test request", &mut kv_data);
+        assert_eq!(kv_data.get("key"), Some(&"test request".to_string()));
+    }
 }
