79b3278451c7eda761ee791b88eede0130fd70b9
Jack Yu <jackysp@gmail.com>
2024-03-05 15:58:41 +0800

    add rocksdb for comparison
    Signed-off-by: Jack Yu <jackysp@gmail.com>

--- benches/engine_benchmark.rs
+++ benches/engine_benchmark.rs
@@ -1,13 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use tegdb::Engine;
 use std::path::PathBuf;
+use rocksdb::DB;
 
 fn engine_benchmark(c: &mut Criterion) {
     let mut engine = Engine::new(PathBuf::from("test.db"));
     let key = b"key";
     let value = b"value";
-    let start_key = b"a";
-    let end_key = b"z";
 
     c.bench_function("engine set", |b| b.iter(|| {
         engine.set(black_box(key), black_box(value.to_vec()));
@@ -17,14 +16,29 @@ fn engine_benchmark(c: &mut Criterion) {
         engine.get(black_box(key));
     }));
 
-    c.bench_function("engine scan", |b| b.iter(|| {
-        engine.scan(black_box(start_key), black_box(end_key));
-    }));
-
     c.bench_function("engine del", |b| b.iter(|| {
         engine.del(black_box(key));
     }));
 }
 
-criterion_group!(benches, engine_benchmark);
+fn rocksdb_benchmark(c: &mut Criterion) {
+    let path = "rocksdb";
+    let db = DB::open_default(path).unwrap();
+    let key = b"key";
+    let value = b"value";
+
+    c.bench_function("rocksdb put", |b| b.iter(|| {
+        db.put(black_box(key), black_box(value)).unwrap();
+    }));
+
+    c.bench_function("rocksdb get", |b| b.iter(|| {
+        db.get(black_box(key)).unwrap();
+    }));
+
+    c.bench_function("rocksdb delete", |b| b.iter(|| {
+        db.delete(black_box(key)).unwrap();
+    }));
+}
+
+criterion_group!(benches, engine_benchmark, rocksdb_benchmark);
 criterion_main!(benches);
