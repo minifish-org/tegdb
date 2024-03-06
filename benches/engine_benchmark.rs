2bc304a246fa99b3da863b8162ba829328dc9d53
Jack Yu <jackysp@gmail.com>
2024-03-06 16:23:45 +0800

    add redb to compare
    Signed-off-by: Jack Yu <jackysp@gmail.com>

--- benches/engine_benchmark.rs
+++ benches/engine_benchmark.rs
@@ -1,6 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use tegdb::Engine;
 use std::path::PathBuf;
+use tegdb::Engine;
+use redb::{Database, ReadableTable, TableDefinition};
 
 fn engine_benchmark(c: &mut Criterion) {
     let mut engine = Engine::new(PathBuf::from("test.db"));
@@ -39,5 +40,38 @@ fn sled_benchmark(c: &mut Criterion) {
     }));
 }
 
-criterion_group!(benches, engine_benchmark, sled_benchmark);
+fn redb_benchmark(c: &mut Criterion) {
+    let path = PathBuf::from("redb");
+    let db = Database::create(path).unwrap();
+    let table_def: TableDefinition<&str, &str> = TableDefinition::new("my_table");
+    
+    let key = "key";
+    let value = "value";
+
+    c.bench_function("redb put", |b| b.iter(|| {
+        let tx = db.begin_write().unwrap();
+        {
+            let mut table = tx.open_table(table_def).unwrap();
+            table.insert(black_box(key), black_box(value)).unwrap();
+        }
+        tx.commit().unwrap();
+    }));
+
+    c.bench_function("redb get", |b| b.iter(|| {
+        let tx = db.begin_read().unwrap();
+        let table = tx.open_table(table_def).unwrap();
+        table.get(black_box(key)).unwrap();
+    }));
+
+    c.bench_function("redb del", |b| b.iter(|| {
+        let tx = db.begin_write().unwrap();
+        {
+            let mut table = tx.open_table(table_def).unwrap();
+            table.remove(black_box(key)).unwrap();
+        }
+        tx.commit().unwrap();
+    }));
+}
+
+criterion_group!(benches, engine_benchmark, sled_benchmark, redb_benchmark);
 criterion_main!(benches);
