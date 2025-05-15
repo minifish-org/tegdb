a619f6c4455f80358038bc6e6cf869624252a11a
Jack Yu <jackysp@gmail.com>
2025-05-15 17:29:44 +0800

    remove redb dependence
    Signed-off-by: Jack Yu <jackysp@gmail.com>

--- benches/engine_basic_benchmark.rs
+++ benches/engine_basic_benchmark.rs
@@ -1,7 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use redb::{Database, ReadableTable, TableDefinition};
 use rusqlite::{params, Connection};
-use tempfile::NamedTempFile;
 use std::path::PathBuf;
 use tegdb::Engine;
 
@@ -80,57 +78,7 @@ fn sled_benchmark(c: &mut Criterion) {
     std::fs::remove_dir_all(path).unwrap();
 }
 
-fn redb_benchmark(c: &mut Criterion) {
-    let db_file = NamedTempFile::new().unwrap();
-    let db = Database::create(db_file.path()).unwrap();
-    let table_def: TableDefinition<&[u8], &[u8]> = TableDefinition::new("my_table");
-    let key = b"key";
-    let value = b"value";
 
-    c.bench_function("redb put", |b| {
-        b.iter(|| {
-            let write_txn = db.begin_write().unwrap();
-            {
-                let mut table = write_txn.open_table(table_def).unwrap();
-                table.insert(black_box(key), black_box(value)).unwrap();
-            }
-            write_txn.commit().unwrap();
-        })
-    });
-
-    c.bench_function("redb get", |b| {
-        b.iter(|| {
-            let read_txn = db.begin_read().unwrap();
-            let table = read_txn.open_table(table_def).unwrap();
-            table.get(black_box(key)).unwrap();
-        })
-    });
-
-    c.bench_function("redb scan", |b| {
-        b.iter(|| {
-            let read_txn = db.begin_read().unwrap();
-            let table = read_txn.open_table(table_def).unwrap();
-            black_box(
-                table
-                    .range(black_box(b"a".as_slice())..black_box(b"z".as_slice()))
-                    .unwrap()
-                    .map(|x| x.unwrap())
-                    .collect::<Vec<_>>()
-            );
-        })
-    });
-
-    c.bench_function("redb del", |b| {
-        b.iter(|| {
-            let write_txn = db.begin_write().unwrap();
-            {
-                let mut table = write_txn.open_table(table_def).unwrap();
-                table.remove(black_box(key)).unwrap();
-            }
-            write_txn.commit().unwrap();
-        })
-    });
-}
 
 fn sqlite_benchmark(c: &mut Criterion) {
     let conn = Connection::open_in_memory().unwrap();
@@ -190,7 +138,6 @@ criterion_group!(
     benches,
     engine_benchmark,
     sled_benchmark,
-    redb_benchmark,
     sqlite_benchmark
 );
 criterion_main!(benches);
--- benches/engine_seq_benchmark.rs
+++ benches/engine_seq_benchmark.rs
@@ -1,5 +1,4 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use redb::{Database, ReadableTable, TableDefinition};
 use std::path::PathBuf;
 use tegdb::Engine;
 
@@ -38,55 +37,7 @@ fn engine_benchmark(c: &mut Criterion, value_size: usize) {
     });
 }
 
-fn redb_benchmark(c: &mut Criterion, value_size: usize) {
-    let db = Database::create("redb.db").unwrap();
-    let table_def = TableDefinition::new("my_table");
-    let value = vec![0; value_size];
 
-    c.bench_function(&format!("redb seq put {}", value_size), |b| {
-        let mut i = 0;
-        b.iter(|| {
-            let key_str = format!("key{}", i);
-            let key = key_str.as_bytes();
-            let write_txn = db.begin_write().unwrap();
-            {
-                let mut table = write_txn.open_table(table_def).unwrap();
-                table.insert(black_box(key), black_box(value.as_slice())).unwrap();
-            }
-            write_txn.commit().unwrap();
-            i += 1;
-        })
-    });
-
-    c.bench_function(&format!("redb seq get {}", value_size), |b| {
-        let mut i = 0;
-        b.iter(|| {
-            let key_str = format!("key{}", i);
-            let key = key_str.as_bytes();
-            let read_txn = db.begin_read().unwrap();
-            let table = read_txn.open_table(table_def).unwrap();
-            table.get(black_box(key)).unwrap();
-            i += 1;
-        })
-    });
-
-    c.bench_function(&format!("redb seq del {}", value_size), |b| {
-        let mut i = 0;
-        b.iter(|| {
-            let key_str = format!("key{}", i);
-            let key = key_str.as_bytes();
-            let write_txn = db.begin_write().unwrap();
-            {
-                let mut table = write_txn.open_table(table_def).unwrap();
-                table.remove(black_box(key)).unwrap();
-            }
-            write_txn.commit().unwrap();
-            i += 1;
-        })
-    });
-
-    std::fs::remove_file("redb.db").unwrap_or_default();
-}
 
 fn sled_benchmark(c: &mut Criterion, value_size: usize) {
     let db = sled::open("sled").unwrap();
@@ -127,13 +78,11 @@ fn sled_benchmark(c: &mut Criterion, value_size: usize) {
 
 fn benchmark_small(c: &mut Criterion) {
     engine_benchmark(c, 1024);
-    redb_benchmark(c, 1024);
     sled_benchmark(c, 1024);
 }
 
 fn benchmark_large(c: &mut Criterion) {
     engine_benchmark(c, 255_000);
-    redb_benchmark(c, 255_000);
     sled_benchmark(c, 255_000);
 }
 
